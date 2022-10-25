62cec3d7e99d501502c5307690b884ca0078952e
Mwero Abdalla <mwerothewebmaker@gmail.com>
2022-10-25 17:52:48 +0300

    Add paddonh yo thr pgwrapper

--- src/Layout/PageWrapper.jsx
+++ src/Layout/PageWrapper.jsx
@@ -2,7 +2,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export const PageWrapper = ({ children }) => {
-  return <div className="p-10 flex flex-row justify-center items-center ">{children}</div>;
+  return (
+    <div className="p-10 flex flex-column gag-10  justify-center items-center ">{children}</div>
+  );
 };
 
 PageWrapper.propTypes = {
