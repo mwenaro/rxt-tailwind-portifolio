3ad16d757177893b66a0aaa5a6f1b84436eda5a1
Mwero Abdalla <mwerothewebmaker@gmail.com>
2022-10-25 17:33:45 +0300

    Add .htacess

--- src/Layout/PageWrapper.jsx
+++ src/Layout/PageWrapper.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export const PageWrapper = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className="p-10 bg-black">{children}</div>;
 };
 
 PageWrapper.propTypes = {
