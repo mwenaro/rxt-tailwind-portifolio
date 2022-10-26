0327b21dc38819d8d4245cdb3fc921b2b774ece0
Mwero Abdalla <mwerothewebmaker@gmail.com>
2022-10-26 10:14:35 +0300

    Styling the nav

--- src/Layout/Nav.jsx
+++ src/Layout/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Nav() {
   return (
-    <nav className="w-1/4 h-20 boorder-3 absolute z-10 bg-gold  ">
+    <nav className="w-1/4 h-20 boorder-3 absolute z-10 bg-gold m-auto">
       <ul className="flex justify-center items-center gap-2">
         <li>Home</li>
         <li>Projects</li>
